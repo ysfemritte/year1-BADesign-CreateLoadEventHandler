658df1c148a051f2f76f8855b8dba19dabeaa6f1
Yusuf Emritte <34180742+ysfemritte@users.noreply.github.com>
2021-09-17 17:47:56 +0400

    Add files via upload

---
+++ Load Event/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Load_Event
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.countriesListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "The Countries of South America:";
+            // 
+            // countriesListBox
+            // 
+            this.countriesListBox.FormattingEnabled = true;
+            this.countriesListBox.ItemHeight = 16;
+            this.countriesListBox.Location = new System.Drawing.Point(35, 58);
+            this.countriesListBox.Name = "countriesListBox";
+            this.countriesListBox.Size = new System.Drawing.Size(194, 116);
+            this.countriesListBox.TabIndex = 1;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(77, 189);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(104, 33);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 239);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.countriesListBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "South America";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox countriesListBox;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Load Event/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Load_Event
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Declare a variable to hold a country name.
+                string countryName;
+
+                // Declare a StreamReader variable.
+                StreamReader inputFile;
+
+                // Open the file and get a StreamReader object.
+                inputFile = File.OpenText("Countries.txt");
+
+                // Read the file's contents.
+                while (!inputFile.EndOfStream)
+                {
+                    // Get a country name.
+                    countryName = inputFile.ReadLine();
+
+                    // Add the country name to the ListBox.
+                    countriesListBox.Items.Add(countryName);
+                }
+
+                // Close the file.
+                inputFile.Close();
+            }
+            catch (Exception ex)
+            {
+                // Display an error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
